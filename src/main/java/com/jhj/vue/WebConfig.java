193ddc15f390e453b93803710a6029acfbd6bb4b
wjsgudwnswjsgudwns <wns10099@naver.com>
2025-10-23 16:46:05 +0900

    수정

--- src/main/java/com/jhj/vue/WebConfig.java
+++ src/main/java/com/jhj/vue/WebConfig.java
@@ -9,15 +9,15 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebConfig {
 	
 	@Bean
-	public WebMvcConfigurer corsConfigurer() {
+	public WebMvcConfigurer corsConfigurer() { //CORS 문제 해결 8081포트에서 오는 요청 개방
 		return new WebMvcConfigurer() {
-
 			@Override
 			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("*/**")
-				.allowedOrigins("http://localhost:8081")
-				.allowedMethods("GET","POST","PUT","DELETE")
-				.allowCredentials(true);
+				registry.addMapping("/**")
+						.allowedOrigins("http://localhost:8081")
+						.allowedMethods("GET","POST","PUT","DELETE")
+//						.allowCredentials(ture); //로그인 인증 필요 -> true 설정 필요
+						;
 			}
 			
 		};
